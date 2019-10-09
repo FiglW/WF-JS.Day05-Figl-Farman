9424808b9becd30acbb7674427ce90c9113cb1f2
Wolfgang Figl <wolfgang.figl@me.com>
2019-10-09 13:23:55 +0200

    hmmm

---
+++ wolfgang/Advanced/script.js
@@ -0,0 +1,15 @@
+var mydata = JSON.parse(data);
+var mydatazwei = JSON.parse(datazwei);
+
+
+  console.log(mydata);
+   console.log(mydatazwei);
+
+
+  document.write("The Book " + mydata[ 0].title+ "from the  "+mydata[0].author+ " is not good to read " + mydata[0 ].read);
+
+    document.write("<hr>"); // new line
+
+   document.write("The Book " + mydatazwei[ 0].title+ "from the  "+mydatazwei[0].author+ " is not good to read " + mydatazwei[0 ].read);
+
+    document.write("<hr>"); // new line
--- wolfgang/intermediate/script.js
+++ wolfgang/intermediate/script.js
@@ -1,13 +1,32 @@
-var mydata = JSON.parse(data); // convert string into an object
+var mydata = JSON.parse(data);
+var mydatazwei = JSON.parse(datazwei);
+var mydatadrei = JSON.parse(datadrei);
+var mydatavier = JSON.parse(datavier); // convert string into an object
 
    console.log(mydata); // prints converted object into the console
+   console.log(mydatazwei);
+   console.log(mydatadrei);
+   console.log(mydatavier);
 
    /* Prints data from Object1 */
 
-   document.write("My name is " + mydata[ 0].name + " and I am working as a " + mydata[0 ].position);
+   document.write("My name is " + mydata[ 0].firstname+ " "+mydata[0].lastname+ " and I am working as a " + mydata[0 ].jobtitle);
 
     document.write("<hr>"); // new line
 
-   /* Prints data from Object1 */
+   /* Prints data from Object2 */
 
-   document.write("My name is " + mydata[ 1].name + " and I am working as a " + mydata[1 ].position); 
+   document.write("My name is " + mydatazwei[ 0].firstname+ " "+mydatazwei[0].lastname+ " and I am working as a " + mydatazwei[0 ].jobtitle);
+
+   document.write("<hr>");
+
+   /*Prints data from Object3 */
+
+   document.write("My name is " + mydatadrei[ 0].firstname+ " "+mydatadrei[0].lastname+ " and I am working as a " + mydatadrei[0 ].jobtitle);
+
+   	document.write("<hr>");
+
+   /*Prints data from Object4 */
+
+   document.write("My name is " + mydatavier[ 0].firstname+ " "+mydatavier[0].lastname+ " and I am working as a " + mydatavier[0 ].jobtitle);
+   	document.write("<hr>")
